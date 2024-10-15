64d6d5c6d469380f6f65eaa4adc40fd92d090ea0
Wonder2018 <49940418+Wonder2018@users.noreply.github.com>
2024-10-15 23:14:47 +0800

    init
    Signed-off-by: Wonder2018 <49940418+Wonder2018@users.noreply.github.com>

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <comp-a v-for="(cell, ind) in new Array()" :key="ind"></comp-a>
+    <comp-a></comp-a>
+    <comp-a></comp-a>
+    <comp-b></comp-b>
+    <comp-b></comp-b>
+    <comp-b></comp-b>
+    <comp-b>
+        <div>ccc</div>
+    </comp-b>
+    <div class="xxx">ddd</div>
+</template>
+
+<script setup lang="ts">
+import CompA from "./CompA.vue";
+import CompB from "./CompB.vue";
+</script>
